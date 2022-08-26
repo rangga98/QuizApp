9f41a09fee84b8d33c8202ae8cc54437d6a86f23
iei20020013 <rangga.pujianto@epson.co.id>
2022-08-26 22:54:47 +0700

    add cors and process data from api in UI

--- QuizAPI/QuizAPI/Program.cs
+++ QuizAPI/QuizAPI/Program.cs
@@ -16,6 +16,11 @@ builder.Services.AddDbContext<QuizDbContext>(options =>
 
 var app = builder.Build();
 
+app.UseCors(options =>
+options.WithOrigins("http://localhost:3000")
+.AllowAnyMethod()
+.AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
